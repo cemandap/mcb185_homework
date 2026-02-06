1f3cbb5e2286e95449dfbed0ba5d810caf56c214
cemandap <cemandap@ucdavis.edu>
2026-02-05 22:04:31 -0800

    demo python script

---
+++ 10demo.py
@@ -0,0 +1,19 @@
+
+# 10demo.py by Cara Mandap
+
+import math
+
+print('hello, again') # greeting
+print(1.5e-2)
+
+print(1+1)
+print(2 ** 3)
+print(pow(2,3))
+print(math.pow(2, 3))
+print(2**0.5)
+print(math.sqrt(2))
+print(math.log(2))
+
+#print(1/0)		# divide by zero error
+#print(math.log(0))	# math domain error
+#print(math.sqrt(-1))	# math domain error 
